51bd36a3c1350fcbf954bf73f5e11ecc9642d6bc
Administrator <1972689216@qq.com>
2020-06-23 20:30:30 +0800

    添加申请操作

--- src/main/java/com/bwsk/entity/DailyStatistic.java
+++ src/main/java/com/bwsk/entity/DailyStatistic.java
@@ -5,64 +5,84 @@ import java.util.List;
 
 public class DailyStatistic extends Project {
 
-	private int dailyTody;// 日报天数
+    private int dailyTody;// 日报天数
 
-	private int dailyTotal;// 日报总数
+    private int dailyTotal;// 日报总数
 
-	private BigDecimal amountodyTotal;// 累计收款
+    private int totalAmountody;//累计收款
 
-	private BigDecimal invoicetodyTotal;// 累计开票
+    private int totalInvoicetody;//累计开票
 
-	private int totalPeople;// 累计人工
+    private BigDecimal amountodyTotal;// 累计收款
 
-	private List<Equipment> equipments;// 装备材料
+    private BigDecimal invoicetodyTotal;// 累计开票
 
-	public BigDecimal getAmountodyTotal() {
-		return amountodyTotal;
-	}
+    private int totalPeople;// 累计人工
 
-	public void setAmountodyTotal(BigDecimal amountodyTotal) {
-		this.amountodyTotal = amountodyTotal;
-	}
+    private List<Equipment> equipments;// 装备材料
 
-	public BigDecimal getInvoicetodyTotal() {
-		return invoicetodyTotal;
-	}
+    public int getTotalAmountody() {
+        return totalAmountody;
+    }
 
-	public void setInvoicetodyTotal(BigDecimal invoicetodyTotal) {
-		this.invoicetodyTotal = invoicetodyTotal;
-	}
+    public void setTotalAmountody(int totalAmountody) {
+        this.totalAmountody = totalAmountody;
+    }
 
-	public List<Equipment> getEquipments() {
-		return equipments;
-	}
+    public int getTotalInvoicetody() {
+        return totalInvoicetody;
+    }
 
-	public void setEquipments(List<Equipment> equipments) {
-		this.equipments = equipments;
-	}
+    public void setTotalInvoicetody(int totalInvoicetody) {
+        this.totalInvoicetody = totalInvoicetody;
+    }
 
-	public int getDailyTody() {
-		return dailyTody;
-	}
+    public BigDecimal getAmountodyTotal() {
+        return amountodyTotal;
+    }
 
-	public void setDailyTody(int dailyTody) {
-		this.dailyTody = dailyTody;
-	}
+    public void setAmountodyTotal(BigDecimal amountodyTotal) {
+        this.amountodyTotal = amountodyTotal;
+    }
 
-	public int getDailyTotal() {
-		return dailyTotal;
-	}
+    public BigDecimal getInvoicetodyTotal() {
+        return invoicetodyTotal;
+    }
 
-	public void setDailyTotal(int dailyTotal) {
-		this.dailyTotal = dailyTotal;
-	}
+    public void setInvoicetodyTotal(BigDecimal invoicetodyTotal) {
+        this.invoicetodyTotal = invoicetodyTotal;
+    }
 
-	public int getTotalPeople() {
-		return totalPeople;
-	}
+    public List<Equipment> getEquipments() {
+        return equipments;
+    }
 
-	public void setTotalPeople(int totalPeople) {
-		this.totalPeople = totalPeople;
-	}
+    public void setEquipments(List<Equipment> equipments) {
+        this.equipments = equipments;
+    }
+
+    public int getDailyTody() {
+        return dailyTody;
+    }
+
+    public void setDailyTody(int dailyTody) {
+        this.dailyTody = dailyTody;
+    }
+
+    public int getDailyTotal() {
+        return dailyTotal;
+    }
+
+    public void setDailyTotal(int dailyTotal) {
+        this.dailyTotal = dailyTotal;
+    }
+
+    public int getTotalPeople() {
+        return totalPeople;
+    }
+
+    public void setTotalPeople(int totalPeople) {
+        this.totalPeople = totalPeople;
+    }
 
 }
